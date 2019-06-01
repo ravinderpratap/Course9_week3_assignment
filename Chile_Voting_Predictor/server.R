f31d51b89f708ef4caae96bd1b918586ae4a0f63
Pratap Singh <r.pratap.singh@accenture.com>
2019-06-01 22:38:58 +0530

    Initial Commit

---
+++ Chile_Voting_Predictor/server.R
@@ -0,0 +1,115 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+#' Chile Prediction model to predict whether person will vote or not?
+#' 
+#' @param df, dataframe to specify the details captured from Application options
+#' @return predict1, Text output containg the prediction for the voter.
+#' 
+library(shiny)
+library(carData)
+data(Chile)
+library(dplyr)
+library(ggplot2)
+library(caret)
+library(e1071)
+library(randomForest)
+
+#function to Predict the voting
+chile_predict <- function(df){
+  
+  chile_data <-  as.data.frame(Chile)
+  chile_data <- na.omit(chile_data) %>%
+    select(-population)  
+  
+  set.seed(5)
+  inTrain = createDataPartition(chile_data$vote, p = 3/4)[[1]]
+  training = chile_data[ inTrain,]
+  validation = chile_data[-inTrain,]
+  
+  train_rf <-  randomForest(vote ~. , data=training, method="class")
+  pred_rf <- predict(train_rf, validation, method="class")
+  
+  percent <- confusionMatrix(pred_rf, validation$vote)$overall["Accuracy"]
+  
+  df <- rbind(validation, df)
+  df <- df[nrow(df),]
+  
+  predict1 <- predict(train_rf, df)
+  
+  if (predict1 == 'A') {
+    predict1 <- "Will Abstain From Voting"
+  }
+  if (predict1 == 'N') {
+    predict1 <- "Will Not Go For Voting"
+  }
+  if (predict1 == 'U') {
+    predict1 <- "Will be Undecided for Voting"
+  }
+  if (predict1 == 'Y') {
+    predict1 <- "Will Go For Voting"
+  }
+  
+  percent <- ceiling(percent * 100)
+  
+  predict1 <- paste("There are",percent,"% chances that Person", predict1)
+  return(predict1)
+  
+}
+
+# Function to plot it
+#' @param df, dataframe to specify the details captured from Application options
+#' @return g1, ggplot output containg the graphical representation of the prediction over existing population.
+#' 
+chile_plot <- function(df){
+  
+  chile_data <-  as.data.frame(Chile)
+  chile_data <- na.omit(chile_data) %>%
+    select(-population)  
+  
+  set.seed(5)
+  inTrain = createDataPartition(chile_data$vote, p = 3/4)[[1]]
+  training = chile_data[ inTrain,]
+  validation = chile_data[-inTrain,]
+  
+  train_rf <-  randomForest(vote ~. , data=training, method="class")
+  pred_rf <- predict(train_rf, validation, method="class")
+  
+  df <- rbind(validation, df)
+  df <- df[nrow(df),]
+  
+  predict1 <- predict(train_rf, df)
+  df$vote <- predict1
+
+  g1 <- ggplot(data = chile_data, aes(x=as.numeric(age), y = as.numeric(income), color = education )) +
+    geom_point() + 
+    facet_grid(vote ~ sex + region) +
+    ylab("Income Per Month") +
+    xlab("Age of Voter") + 
+    ggtitle("Voting predictor - Prediction shown in Black Color")
+  
+  g1 <- g1 + geom_point(data = df, color = "black", size=3)
+  
+  return(g1)
+  
+}
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$prediction <- renderPrint({
+    df <- c(input$region, input$sex,input$age, input$education,input$income, input$statusquo, 'N')
+    chile_predict(df)
+    })
+  
+  output$distPlot <- renderPlot({
+    df <- c(input$region, input$sex,input$age, input$education,input$income, input$statusquo, 'N')
+    chile_plot(df)
+  })
+  
+})
---
+++ Chile_Voting_Predictor/ui.R
@@ -0,0 +1,52 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Chile Voting Predictor"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      numericInput('age', 'Age of Voter', 25, min = 18, max = 100, step = 1),
+      numericInput('income', 'Monthly Income of Person', 35000, min = 25000, max = 250000, step = 5000),
+      radioButtons('region', "Region of Person",
+                   c("Central" = "C",
+                     "Metropolitan Santiago Area" = "M",
+                     "North" = "N",
+                     "South" = "S",
+                     "Santiago City" = "SA")),
+      radioButtons('sex', "Sex of Person",
+                   c("Female" = "F",
+                     "Male" = "M")),
+      numericInput('statusquo', 'Statusquo of Voter', 1, min = -1.72, max = 1.72, step = .2),
+      radioButtons('education', "Education of Person",
+                   c("Primary" = "P",
+                     "Post-secondary" = "PS",
+                     "Secondary" = "S")),
+      submitButton('Submit'),
+      width = 3
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       h3('Results of the Prediction is'),
+       verbatimTextOutput("prediction"),
+       h3('Plots for the existing distribution of Chile Popluation'),
+       plotOutput("distPlot"),
+       h5("Possible outcomes of Predications are:" , 
+          strong("A - Abstain from Voting, N - Will Not Vote, U - Undecided, Y - Will Vote")),
+       width = 9
+    )
+  )
+))
